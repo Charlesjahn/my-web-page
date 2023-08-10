21f9b82000bc9f7b1ecc225c422e2a0742e4f16b
Charles Franklin Jahn <charlesjahn@gmail.com>
2023-08-10 23:57:26 +0100

    update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,4 @@
-// import styles from './Home.module.css'
+import styles from './Home.module.css'
 import myImg from '../../img/me_thinking.png'
 
 import Img from '../layout/Img'
@@ -6,13 +6,19 @@ import Img from '../layout/Img'
 function Home() {
 
     return (
-        <section>
-            <div >
-                <h1>Charles </h1>
-                <h1>Franklin </h1>
-                <h1>Jahn</h1>
+        <section className={styles.section}>
+            <div>
+                <h1>Hello World!</h1>
+                <p>I`m </p>
+                <div >
+                    <h2>Charles </h2>
+                    <h2>Franklin </h2>
+                    <h2>Jahn</h2>
+                </div>
+                <p>An emerging Full Stack Developer. Enthusiastic in React, HTML, CSS, JavaScript, Python, Node.js, Java and more...</p>
             </div>
-            <Img src={myImg} alt={"Myself"}/>
+
+            <Img src={myImg} alt={"Myself"} />
         </section>
     )
 }
