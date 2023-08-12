67c3440b043e4888d9f2783a5fd46ab0864ead80
Charles Franklin Jahn <charlesjahn@gmail.com>
2023-08-12 21:12:34 +0100

    Update TimelineElements.js

--- src/components/layout/TimelineElements.js
+++ src/components/layout/TimelineElements.js
@@ -6,6 +6,7 @@ let timeline = [
         description: "I was born",
         date: "February 17th, 1993",
         icon: "born",
+        type: ""
     },
     {
         id: 2,
@@ -14,6 +15,7 @@ let timeline = [
         description: "Elementary school from 1999-2008 & High school from 2009 to 2011",
         date: "February 1999 to December 2011",
         icon: "school",
+        type: "education"
     },
     {
         id: 3,
@@ -22,6 +24,7 @@ let timeline = [
         description: "An intensive Industrial Mechanic Technician course that covered a diverse range of subjects such as equipment maintenance, troubleshooting, assembly, and repair techniques. Which included both hands-on practical training and theoretical coursework, in analyzing technical problems, utilizing specialized tools, and implementing safety protocols in industrial environments. Able me to make valuable contributions in maintenance operations, process enhancement, and collaborative teamwork within the industrial sector.",
         date: "February 2009 to November 2010",
         icon: "school",
+        type: "education"
     },
     {
         id: 4,
@@ -30,6 +33,7 @@ let timeline = [
         description: "Through this comprehensive course, I gained hands-on experience and a deep understanding of the fundamental concepts, techniques, and tools used in both AutoCAD and SolidWorks. Proficient in producing accurate 2D and 3D technical drawings and models using AutoCAD and SolidWorks, with a strong grasp of drafting standards, dimensioning, and annotation practices. ",
         date: "January 2011",
         icon: "course",
+        type: "education"
     },
     {
         id: 5,
@@ -38,6 +42,7 @@ let timeline = [
         description: "Assisted in troubleshooting, repairs and maintenance of industrial machinery and equipment. Contributes to routine inspections and performs basic and deep mechanical tasks to ensure optimal operating efficiency.",
         date: "February 2011 - March 2012",
         icon: "work",
+        type: "profession"
     },
     {
         id: 6,
@@ -46,6 +51,7 @@ let timeline = [
         description: "Multidisciplinary area that allows students to learn and apply scientific methods (data science, databases, software engineering, computer architecture, computer graphics, computer and smartphone programming, compilers) to build software and/or applications. Analyzes, designs and develops computational systems, applied to different areas of knowledge. Did not complete due to financial problems.",
         date: "February 2012 - June 2013",
         icon: "graduation",
+        type: "education"
     },
     {
         id: 7,
@@ -54,6 +60,7 @@ let timeline = [
         description: "Assisted in the installation of security systems, diagnosis, repair and maintenance of hydraulic and pneumatic systems. I actively participated in equipment installations, consistently respecting safety protocols. Gained valuable hands-on experience in a dynamic industrial environment, focusing on the complex mechanics of hydraulic and pneumatic technologies.",
         date: "November 2012 - May 2013",
         icon: "work",
+        type: "profession"
     },
     {
         id: 8,
@@ -62,6 +69,7 @@ let timeline = [
         description: "Certified in lifesaving techniques including CPR, wound care and emergency response. Ready to provide critical assistance in high-pressure situations, contributing to a safer environment.",
         date: "May 2013",
         icon: "course",
+        type: "education"
     },
     {
         id: 9,
@@ -70,6 +78,7 @@ let timeline = [
         description: "It conceives forms of modernization of products or processes through design, execution and implementation of new industrial process controls. It develops new products or solutions with regard to automation, in the search for the reduction of human intervention in the operation of machines and equipment, in the development of products that facilitate human mobility. Did not complete due to an exchange plan to Ireland.",
         date: "February 2015 - June 2016",
         icon: "graduation",
+        type: "education"
     },
     {
         id: 10,
@@ -78,6 +87,7 @@ let timeline = [
         description: "Industrial Mechanical Technician with experience in hydraulic and pneumatic industrial machines. Skilled in installing, maintaining and repairing complex mechanical systems. Proven experience in troubleshooting and diagnosing problems to ensure optimal operator and machine functionality and safety. Adept at reading blueprints and technical schematics.",
         date: "May 2013 - February 2018",
         icon: "work",
+        type: "profession"
     },
     {
         id: 11,
@@ -86,6 +96,7 @@ let timeline = [
         description: "Ireland, here I go!!!",
         date: "April 2018",
         icon: "travel",
+        type: ""
     },
     {
         id: 12,
@@ -94,6 +105,7 @@ let timeline = [
         description: "Started my English language course, where I started from A2 lvl  and finished it C1 lvl.",
         date: "April 2018 - February 2020",
         icon: "course",
+        type: "education"
     },
     {
         id: 13,
@@ -102,6 +114,7 @@ let timeline = [
         description: "Proficient in all aspects of property maintenance, ensuring safe, comfortable and well-functioning living environments. Proven ability to quickly identify and resolve maintenance issues to minimize disruptions and increase resident satisfaction. Committed to maintaining high standards of cleanliness, safety and functionality in student accommodation.",
         date: "June 2018 - April 2020",
         icon: "work",
+        type: "profession"
     },
     {
         id: 14,
@@ -110,6 +123,7 @@ let timeline = [
         description: "Completed the comprehensive barista course, mastering the art of crafting exceptional coffee drinks. Experience developed in the preparation of espresso and milk froth techniques. Trained in providing top-notch customer service, ensuring a welcoming and enjoyable cafe experience.",
         date: "November 2019",
         icon: "course",
+        type: "education"
     },
     {
         id: 15,
@@ -118,6 +132,7 @@ let timeline = [
         description: "I provided excellent customer service by ensuring proper bike maintenance, fit, and safety. During the first year, I introduced the Trello system to the owner, which led to improved visualization of our services, minimized delays, and a reduction in the number of complaints.",
         date: "May 2020 - June 2021",
         icon: "work",
+        type: "profession"
     },
     {
         id: 16,
@@ -126,6 +141,7 @@ let timeline = [
         description: "It conceives forms of modernization of products or processes through design, execution and implementation of new industrial process controls. It develops new products or solutions with regard to automation, in the search for the reduction of human intervention in the operation of machines and equipment, in the development of products that facilitate human mobility.",
         date: "September 2020 - Present",
         icon: "graduation",
+        type: "education"
     },
     {
         id: 17,
@@ -134,6 +150,7 @@ let timeline = [
         description: "Assuming the role of Chief Mechanic and Technical Manager, I was responsible for overseeing bike services, managing parts inventory and ensuring exceptional customer service. During this time, I honed strong communication and negotiation skills. Introduced an innovative approach to customer service and implemented a new checklist methodology, resulting in better customer satisfaction rates and reduced losses throughout the process.",
         date: "June 2021 - May 2023",
         icon: "work",
+        type: "profession"
     }, 
     {
         id: 18,
@@ -142,6 +159,7 @@ let timeline = [
         description: "Helping fisrt year students in tasks of Java, JavaScript, HTML, CSS and Maths",
         date: "September 2021 - Present",
         icon: "work",
+        type: "profession"
     }, 
     {
         id: 19,
@@ -150,6 +168,7 @@ let timeline = [
         description: "A voluntary role where I am enhancing my web development skills by working with WordPress for maintenance and optimization, ensuring a seamless user experience across various devices. Additionally, I remotely managed the radio stream server, performing regular checks and updates to ensure uninterrupted broadcasting.",
         date: "August 2023 - Present",
         icon: "work",
+        type: "profession"
     }
 ]
 
