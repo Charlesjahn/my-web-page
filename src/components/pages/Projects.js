2c6e8d8236d43224a8de93870916387f6f6498fa
Charles Franklin Jahn <charlesjahn@gmail.com>
2023-08-12 15:08:05 +0100

    Projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -13,29 +13,66 @@ function Projects() {
     })
         .then((reponse) => reponse.json())
         .then(data => setProjects(data))
-        .catch((err) => { console.log(err) })
+        .catch((err) => {
+            console.log(err)
+        })
+
+    function checkType(type) {
+        const typeLang = {
+            CSS: "JavaScript",
+            HTML: "JavaScript",
+        };
+
+        const languageType = typeLang[type] || type;
+
+        return languageType;
+    }
+
+    function capitalizeTitle(title) {
+        return title.replace(/(?:^|\s)\w/g, firstLetter => firstLetter.toUpperCase());
+    }
 
 
     return (
-        <section className={styles.section_projects}>
+        <section>
             <h1>Projects</h1>
             {projects.length === 0 ? (
                 <p>Loading...</p>
             ) : (
-                <div>
-                    {projects.map(project => (
-                        <div key={project.id}>
-                            <a href={project.html_url} target="_blank" rel="noopener noreferrer">
-                            Github link of {project.name}
-                            </a>
-                            {project.has_pages && (
-                                <a href={`https://charlesjahn.github.io/${project.name}/`} target="_blank" rel="noopener noreferrer">
-                                    {project.name}
-                                </a>
-                            )}
-                        </div>
-                    ))}
-                </div>
+                <table className={styles.table}>
+                    <thead>
+                        <tr className={styles.title_list}>
+                            <th>Code link</th>
+                            <th>Language</th>
+                            <th>Page</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {projects.map(project => (
+                            project.name.toUpperCase() !== "CHARLESJAHN" && (
+                                <tr key={project.id} className={styles.table_row}>
+                                    <td>
+                                        <a href={project.html_url} target="_blank" rel="noopener noreferrer">
+                                            {capitalizeTitle(project.name)}
+                                        </a>
+                                    </td>
+                                    <td>
+                                        {project.language && (
+                                            <p>{checkType(project.language)}</p>
+                                        )}
+                                    </td>
+                                    <td>
+                                        {project.has_pages && (
+                                            <a href={`https://charlesjahn.github.io/${project.name}/`} target="_blank" rel="noopener noreferrer">
+                                                {project.name}
+                                            </a>
+                                        )}
+                                    </td>
+                                </tr>
+                            )))}
+                    </tbody>
+                </table>
+
             )}
         </section>
     )
