30afb7e0c0c372ea244e437d9f53dba90a193819
Charles Franklin Jahn <charlesjahn@gmail.com>
2023-08-12 13:51:13 +0100

    Update Projects.js

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,10 +1,42 @@
 import styles from './Projects.module.css'
 
+import React, { useState, useEffect } from 'react';
+
 function Projects() {
 
+    const [projects, setProjects] = useState([]);
+    const urlGitHub = 'https://api.github.com/users/Charlesjahn/repos'
+
+
+    fetch(urlGitHub, {
+        method: 'GET'
+    })
+        .then((reponse) => reponse.json())
+        .then(data => setProjects(data))
+        .catch((err) => { console.log(err) })
+
+
     return (
-        <section>
+        <section className={styles.section_projects}>
             <h1>Projects</h1>
+            {projects.length === 0 ? (
+                <p>Loading...</p>
+            ) : (
+                <div>
+                    {projects.map(project => (
+                        <div key={project.id}>
+                            <a href={project.html_url} target="_blank" rel="noopener noreferrer">
+                            Github link of {project.name}
+                            </a>
+                            {project.has_pages && (
+                                <a href={`https://charlesjahn.github.io/${project.name}/`} target="_blank" rel="noopener noreferrer">
+                                    {project.name}
+                                </a>
+                            )}
+                        </div>
+                    ))}
+                </div>
+            )}
         </section>
     )
 }
