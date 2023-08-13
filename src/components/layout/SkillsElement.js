f9e55814e3427c47853590970d76533fe2b352f8
Charles Franklin Jahn <charlesjahn@gmail.com>
2023-08-13 19:06:12 +0100

    Update SkillsElement.js

--- src/components/layout/SkillsElement.js
+++ src/components/layout/SkillsElement.js
@@ -3,7 +3,7 @@ let skillsElements = [
     {
         id: 1,
         skill: "React",
-        level: 80,
+        level: 50,
     },
     {
         id: 2,
@@ -12,36 +12,45 @@ let skillsElements = [
     },
     {
         id: 3,
-        skill: "HTML",
-        level: 80,
+        skill: "HTML/CSS",
+        level: 90,
     },
     {
         id: 4,
-        skill: "CSS",
-        level: 80,
+        skill: "Java",
+        level: 75,
     },
     {
         id: 5,
-        skill: "Java",
-        level: 80,
+        skill: "Python",
+        level: 70,
     },
     {
         id: 6,
-        skill: "Python",
-        level: 80,
+        skill: "Spring Boots",
+        level: 50,
     },
+
     {
         id: 7,
-        skill: "Spring Boots",
+        skill: "Portugues",
+        level: 99,
+    },
+    {
+        id: 8,
+        skill: "English",
         level: 80,
     },
     {
         id: 8,
-        skill: "CSS",
-        level: 80,
+        skill: "Spanish",
+        level: 40,
     },
-    
+    {
+        id: 8,
+        skill: "German",
+        level: 20,
+    },
 ]
-
 
 export default skillsElements
