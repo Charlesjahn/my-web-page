ea1899ef1a522a2c75e09644fe8fe6217f8978f0
Charles Franklin Jahn <charlesjahn@gmail.com>
2023-08-15 20:44:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import React from 'react'
+
 import NavBar from './components/navbar/NavBar'
 import Footer from './components/footer/Footer'
 
 import Home from './components/pages/Home'
-import Contact from './components/pages/Contact'
 import Timeline from './components/pages/Timeline'
 import Projects from './components/pages/Projects'
 import Container from './components/layout/Container'
 import Skills from './components/pages/Skills'
 
 function App() {
+
   return (
+    
     <React.Fragment>
       <Router>
         <NavBar />
@@ -22,15 +24,14 @@ function App() {
 
             <Route path="/timeline" element={<Timeline />} />
 
-            <Route path="/contact" element={<Contact />} />
-
             <Route path="/projects" element={<Projects />} />
             
             <Route path="/skills" element={<Skills />} />
 
           </Routes>
         </Container>
-        <Footer />
+
+        <Footer/>
 
       </Router>
     </React.Fragment>
