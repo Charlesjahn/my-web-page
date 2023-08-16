541482bd6c9f3f4ecd5bdc35d68b00456569ee23
Charles Franklin Jahn <charlesjahn@gmail.com>
2023-08-16 17:55:20 +0100

    update

--- src/components/layout/TimelineElements.js
+++ src/components/layout/TimelineElements.js
@@ -1,15 +1,6 @@
 let timeline = [
     {
-        id: 1,
-        title: "Date of Birth",
-        location: "Caxias do Sul - Rio Grande do Sul, Brasil",
-        description: "I was born",
-        date: "February 17th, 1993",
-        icon: "born",
-        type: ""
-    },
-    {
-        id: 2,
+        id: 1, 
         title: "School",
         location: "Caxias do Sul - Rio Grande do Sul, Brasil",
         description: "Elementary school from 1999-2008 & High school from 2009 to 2011",
@@ -18,7 +9,7 @@ let timeline = [
         type: "education"
     },
     {
-        id: 3,
+        id: 2,
         title: "Industrial Mechanic Technician",
         location: "Senai Nilo Pecanha Caxias do Sul - RS, Br",
         description: "An intensive Industrial Mechanic Technician course that covered a diverse range of subjects such as equipment maintenance, troubleshooting, assembly, and repair techniques. Which included both hands-on practical training and theoretical coursework, in analyzing technical problems, utilizing specialized tools, and implementing safety protocols in industrial environments. Able me to make valuable contributions in maintenance operations, process enhancement, and collaborative teamwork within the industrial sector.",
@@ -27,16 +18,16 @@ let timeline = [
         type: "education"
     },
     {
-        id: 4,
+        id: 3,
         title: "AutoCAD & SolidWorks",
         location: "Senai Nilo Pecanha Caxias do Sul - RS, Br",
         description: "Through this comprehensive course, I gained hands-on experience and a deep understanding of the fundamental concepts, techniques, and tools used in both AutoCAD and SolidWorks. Proficient in producing accurate 2D and 3D technical drawings and models using AutoCAD and SolidWorks, with a strong grasp of drafting standards, dimensioning, and annotation practices. ",
         date: "January 2011",
         icon: "course",
-        type: "education"
+        type: "tech"
     },
     {
-        id: 5,
+        id: 4,
         title: "Industrial Mechanic Assistant",
         location: "MP Metallic Structure Caxias do Sul - RS, Br",
         description: "Assisted in troubleshooting, repairs and maintenance of industrial machinery and equipment. Contributes to routine inspections and performs basic and deep mechanical tasks to ensure optimal operating efficiency.",
@@ -45,16 +36,7 @@ let timeline = [
         type: "profession"
     },
     {
-        id: 6,
-        title: "Bachelor of Computer Science",
-        location: "University of Caxias do Sul - RS, Br",
-        description: "Multidisciplinary area that allows students to learn and apply scientific methods (data science, databases, software engineering, computer architecture, computer graphics, computer and smartphone programming, compilers) to build software and/or applications. Analyzes, designs and develops computational systems, applied to different areas of knowledge. Did not complete due to financial problems.",
-        date: "February 2012 - June 2013",
-        icon: "graduation",
-        type: "education"
-    },
-    {
-        id: 7,
+        id: 5,
         title: "Industrial Mechanic Assistant",
         location: "AEtech Máquinas e Segurança Industrial Ltda Caxias do Sul - RS, Br",
         description: "Assisted in the installation of security systems, diagnosis, repair and maintenance of hydraulic and pneumatic systems. I actively participated in equipment installations, consistently respecting safety protocols. Gained valuable hands-on experience in a dynamic industrial environment, focusing on the complex mechanics of hydraulic and pneumatic technologies.",
@@ -63,7 +45,7 @@ let timeline = [
         type: "profession"
     },
     {
-        id: 8,
+        id: 6,
         title: "First Aid",
         location: "SESI Caxias do Sul - RS, Br",
         description: "Certified in lifesaving techniques including CPR, wound care and emergency response. Ready to provide critical assistance in high-pressure situations, contributing to a safer environment.",
@@ -72,16 +54,7 @@ let timeline = [
         type: "education"
     },
     {
-        id: 9,
-        title: "Bachelor of Control and Automation Engineering",
-        location: "University of Caxias do Sul - RS, Br",
-        description: "It conceives forms of modernization of products or processes through design, execution and implementation of new industrial process controls. It develops new products or solutions with regard to automation, in the search for the reduction of human intervention in the operation of machines and equipment, in the development of products that facilitate human mobility. Did not complete due to an exchange plan to Ireland.",
-        date: "February 2015 - June 2016",
-        icon: "graduation",
-        type: "education"
-    },
-    {
-        id: 10,
+        id: 7,
         title: "Industrial Mechanical Technician",
         location: "AEtech Máquinas e Segurança Industrial Ltda Caxias do Sul - RS, Br",
         description: "Industrial Mechanical Technician with experience in hydraulic and pneumatic industrial machines. Skilled in installing, maintaining and repairing complex mechanical systems. Proven experience in troubleshooting and diagnosing problems to ensure optimal operator and machine functionality and safety. Adept at reading blueprints and technical schematics.",
@@ -90,25 +63,16 @@ let timeline = [
         type: "profession"
     },
     {
-        id: 11,
-        title: "Exchange to Ireland",
-        location: "Brasil to Ireland",
-        description: "Ireland, here I go!!!",
-        date: "April 2018",
-        icon: "travel",
-        type: ""
-    },
-    {
-        id: 12,
+        id: 10,
         title: "English Course",
         location: "Erin School of English - Irl",
         description: "Started my English language course, where I started from A2 lvl  and finished it C1 lvl.",
         date: "April 2018 - February 2020",
         icon: "course",
-        type: "education"
+        type: ""
     },
     {
-        id: 13,
+        id: 11,
         title: "Residential Maintenance Technician",
         location: "Egali Exchange - Dublin, Irl",
         description: "Proficient in all aspects of property maintenance, ensuring safe, comfortable and well-functioning living environments. Proven ability to quickly identify and resolve maintenance issues to minimize disruptions and increase resident satisfaction. Committed to maintaining high standards of cleanliness, safety and functionality in student accommodation.",
@@ -117,7 +81,7 @@ let timeline = [
         type: "profession"
     },
     {
-        id: 14,
+        id: 12,
         title: "Barista",
         location: "Eric Levy - Dublin",
         description: "Completed the comprehensive barista course, mastering the art of crafting exceptional coffee drinks. Experience developed in the preparation of espresso and milk froth techniques. Trained in providing top-notch customer service, ensuring a welcoming and enjoyable cafe experience.",
@@ -126,7 +90,7 @@ let timeline = [
         type: "education"
     },
     {
-        id: 15,
+        id: 13,
         title: "Bicycle Mechanic",
         location: "Life in Motion Bike Shop - Dublin",
         description: "Provide excellent customer service by ensuring proper bike maintenance, fit, and safety. During the first year. Introduce and implement Trello, which improved visualisation of our services, making it more efficient and bringing higher customer satisfaction.",
@@ -135,40 +99,40 @@ let timeline = [
         type: "profession"
     },
     {
-        id: 16,
+        id: 14,
         title: "Bachelor of Computer Science",
         location: "CCT College Dublin",
         description: "It conceives forms of modernization of products or processes through design, execution and implementation of new industrial process controls. It develops new products or solutions with regard to automation, in the search for the reduction of human intervention in the operation of machines and equipment, in the development of products that facilitate human mobility.",
         date: "September 2020 - Present",
         icon: "graduation",
-        type: "education"
+        type: "tech"
     },
     {
-        id: 17,
+        id: 15,
         title: "Team Leader / Manager",
         location: "Life in Motion Bike Shop - Dublin",
         description: "Assuming the role of Chief Mechanic and Technical Manager, I was responsible for overseeing bike services, managing parts inventory and ensuring exceptional customer service. During this time, I honed strong communication and negotiation skills. Introduced an innovative approach to customer service and implemented a new checklist methodology, resulting in better customer satisfaction rates and reduced losses throughout the process.",
         date: "June 2021 - May 2023",
         icon: "work",
         type: "profession"
-    }, 
+    },
     {
-        id: 18,
+        id: 16,
         title: "Mentor",
         location: "CCT College Dublin",
         description: "Helped first-year students with tasks, studies, and the fundamentals of Java, JavaScript, HTML, CSS, and Math.",
         date: "September 2021 - Present",
         icon: "work",
-        type: "profession"
-    }, 
+        type: "tech"
+    },
     {
-        id: 19,
+        id: 17,
         title: "Web Developer Intern",
         location: "Babylon Radio Dublin",
         description: "A voluntary role where I am enhancing my web development skills by working with WordPress for maintenance and optimization, ensuring a seamless user experience across various devices. Additionally, I remotely managed the radio stream server, performing regular checks and updates to ensure uninterrupted broadcasting.",
         date: "August 2023 - Present",
         icon: "work",
-        type: "profession"
+        type: "tech"
     }
 ]
 
