99034607cd14d38f3ce3da93808d14518ee3f020
Charles Franklin Jahn <145346870+CharlesCCT2020315@users.noreply.github.com>
2023-11-22 13:28:36 +0000

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,6 @@ import myImg from '../../img/me/me_thinking.png'
 import Img from '../layout/Img'
 
 function Home() {
-
     return (
         <section className={styles.section}>
             <div className={styles.text_home}>
@@ -21,7 +20,15 @@ function Home() {
                 <p>Originally from Brazil and currently based in Ireland for the past 5 years. I'm a hardworking and dedicated individual committed to achieving my goals and building a successful future.</p>
 
                 <p>I'm an aspiring Full Stack Developer, eager to expand and refine my skillset. Using technologies like React, HTML, CSS, and JavaScript. I'm also proficient in Python, Node.js, Java, SpringBoots, and more, always excited to delve into new languages and frameworks to broaden my expertise.</p>
-
+                <div className={styles.divbtn}>
+                    <a
+                        href="https://drive.google.com/file/d/1a2Qdim6jN9ywFR7XW_2U1EYrq1sL-sNw/view?usp=sharing"
+                        download="CharlesJahnCV.pdf"
+                        rel="noopener noreferrer"
+                        >
+                            Download CV
+                    </a>
+                </div>
             </div>
 
             <div className={styles.img_div}>
